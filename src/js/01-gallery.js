775d43879ed5d6c4d0b4ef989c396e9bb4732dc4
Sergii Tatianchenko <taurus090@gmail.com>
2022-09-27 23:35:26 +0300

    ref code gallery-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,26 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryMarkup = createGalleryMarkup(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+          <a class="gallery__item" href="${original}">
+	<img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+    })
+    .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
