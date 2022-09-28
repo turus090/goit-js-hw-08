f6bf0a33da6bfcf94da10660990dd1d1a392a36a
Sergii Tatianchenko <taurus090@gmail.com>
2022-09-28 09:29:23 +0300

    js gallery 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function ({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', `${seconds}`);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+
+player.setCurrentTime(currentTime);
