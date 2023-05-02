08eb8c64efd32391e20c53e26383dc5fe0bb9377
Sergii Tatianchenko <taurus090@gmail.com>
2023-05-02 18:34:37 +0300

    add js/03-feedback.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,12 +4,14 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const onPlay = function ({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', `${seconds}`);
-};
+  if (localStorage.getItem('current-time')){
+    player.setCurrentTime(parseFloat(localStorage.getItem('current-time')))
+  }
 
-player.on('timeupdate', throttle(onPlay, 1000));
 
-const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
+player.on(
+  'timeupdate',
+   throttle((e)=>{
+    localStorage.setItem('current-time', e.seconds)
+}, 1000));
 
-player.setCurrentTime(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle'
+const key ="feedback-form-state"
+const feedback = document.querySelector('.feedback-form')
+const form = { 
+    email: document.querySelector('input'),
+    message: document.querySelector('textarea')
+}
+let draftInputs = {}
 
+const setItems = () => {
+    form.email.value = draftInputs.email || null
+        form.message.value = draftInputs.message || null
+}
+const setForms  =() => { 
+    if (localStorage.getItem(key)){
+        draftInputs = JSON.parse(localStorage.getItem(key))
+        setItems()
+    } else {
+        draftInputs = {}
+        setItems()
+    }
+}
+
+setForms()
+
+hadleUpdate = (e)=>{
+    draftInputs[e.target.name] = e.target.value
+    localStorage.setItem(key,JSON.stringify(draftInputs))
+}
+
+feedback.addEventListener('input', throttle(hadleUpdate,500))
+feedback.addEventListener('submit', () =>{
+    localStorage.removeItem(key)
+    setForms()
+})
